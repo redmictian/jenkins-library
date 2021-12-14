b4720b374e81fc2381e12bea86a17249e4e53ad9
redmictian <redmictian@gmail.com>
2021-12-14 20:36:56 +0300

    vars are added

---
+++ vars/buildImage.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+def call(String imageName) {
+	echo "building the docker image"
+	withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+		sh "docker build -t redmictian/$imageName:$VERSION ."
+		sh "echo $PASS | docker login -u $USER --password-stdin"
+		sh "docker push redmictian/$imageName:$VERSION"
+	}
+}
